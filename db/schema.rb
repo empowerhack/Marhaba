7071353c1780e4e877ca89bcd9002d3abecefabf
Denise <yu.denise.d@gmail.com>
2015-11-28 20:57:13 +0000

    add user data fields to subscriber model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128204603) do
+ActiveRecord::Schema.define(version: 20151128205439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,9 @@ ActiveRecord::Schema.define(version: 20151128204603) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "community_id"
+    t.string   "name"
+    t.string   "mobile"
+    t.string   "email"
   end
 
   add_index "subscribers", ["community_id"], name: "index_subscribers_on_community_id", using: :btree
