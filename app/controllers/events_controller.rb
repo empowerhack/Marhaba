9093435f24fd1d4ef99f0852ebfe94fc545228a2
giuseppe de santis <giusdesan@gmail.com>
2015-11-29 15:51:05 +0000

    almost finished events main page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.where(:date_and_time > DateTime.now).order(:date_and_time)
   end
 
   # GET /events/1
