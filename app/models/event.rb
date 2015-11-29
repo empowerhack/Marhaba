6353d729590358180a9a528aa135aba8e55c64eb
Denise <yu.denise.d@gmail.com>
2015-11-29 17:32:03 +0000

    fixing some views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :host
   belongs_to :community
+  validates_presence_of :title
+  validates_presence_of :date_and_time
+  validates_presence_of :location
+  validates_presence_of :community_id
 
   scope :later_than_today, lambda{ where("date_and_time > ?", Date.today) }
 end
